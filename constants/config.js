4ea14d1a8e2a273542e3a88a776f142e473479d6
maniesh_m <64514465+git-maniesh@users.noreply.github.com>
2025-06-05 08:40:02 +0530

    Update config.js

--- constants/config.js
+++ constants/config.js
@@ -1,5 +1,9 @@
 const corsOptions = {
-  origin: '*',
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:4173",
+    process.env.CLIENT_URL,
+  ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
