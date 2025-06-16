eb2ec65026f7fc4ace2e074c37c07b1b38e05299
maniesh_m <64514465+git-maniesh@users.noreply.github.com>
2025-06-16 23:01:06 +0530

    Update chat.js

--- controllers/chat.js
+++ controllers/chat.js
@@ -37,42 +37,7 @@ const newGroupChat = TryCatch(async (req, res, next) => {
   });
 });
 
-const getMyChats = TryCatch(async (req, res, next) => {
-  const chats = await Chat.find({ members: req.user }).populate(
-    "members",
-    "name avatar"
-  );
 
-  const transformedChats = chats.map(({ _id, name, members, groupChat }) => {
-    const otherMember = getOtherMember(members, req.user);
-    // console.log(members)
-    // console.log("hello")
-    // console.log(otherMember)
-    // console.log("user", req.user)
-
-    return {
-      _id,
-      groupChat,
-      avatar: groupChat
-        ? members.slice(0, 3).map(({ avatar }) => avatar.url)
-        : [otherMember?.avatar?.url],
-      name: groupChat ? name : otherMember.name,
-      // name: groupChat ? name :"",
-
-      members: members.reduce((prev, curr) => {
-        if (curr._id.toString() !== req.user.toString()) {
-          prev.push(curr._id);
-        }
-        return prev;
-      }, []),
-    };
-  });
-
-  return res.status(200).json({
-    success: true,
-    chats: transformedChats,
-  });
-});
 
 const getMyGroups = TryCatch(async (req, res, next) => {
   const chats = await Chat.find({
@@ -93,6 +58,39 @@ const getMyGroups = TryCatch(async (req, res, next) => {
     groups,
   });
 });
+
+
+const getMyChats = TryCatch(async (req, res, next) => {
+  const chats = await Chat.find({ members: req.user }).populate(
+    "members",
+    "name avatar"
+  );
+
+  const transformedChats = chats.map(({ _id, name, members, groupChat }) => {
+    const otherMember = getOtherMember(members, req.user);
+
+    return {
+      _id,
+      groupChat,
+      avatar: groupChat
+        ? members.slice(0, 3).map(({ avatar }) => avatar.url)
+        : [otherMember.avatar.url],
+      name: groupChat ? name : otherMember.name,
+      members: members.reduce((prev, curr) => {
+        if (curr._id.toString() !== req.user.toString()) {
+          prev.push(curr._id);
+        }
+        return prev;
+      }, []),
+    };
+  });
+
+  return res.status(200).json({
+    success: true,
+    chats: transformedChats,
+  });
+});
+
 
 const addMembers = TryCatch(async (req, res, next) => {
   const { chatId, members } = req.body;
